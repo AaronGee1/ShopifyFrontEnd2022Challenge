70119a25a936ea630e3e342da23d090d373aeb20
AaronGee1 <aaron.gee@ryerson.ca>
2022-05-10 03:44:17 +0000

    Added key to list of responses

--- src/Responses.js
+++ src/Responses.js
@@ -1,8 +1,8 @@
 import { Container, Row } from "reactstrap";
 
 const Responses = (props) => {
-  const responseList = props.responseList.map((response) => (
-    <Container className="bg-info mt-2 p-4">
+  const responseList = props.responseList.map((response, index) => (
+    <Container key={index} className="bg-info mt-2 p-4">
       <Row>Prompt: {response.prompt}</Row>
       <Row>Response: {response.response}</Row>
     </Container>
