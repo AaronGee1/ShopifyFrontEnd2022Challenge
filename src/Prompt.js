91ebdb5a55f757cea74499362700dc78bb173f87
AaronGee1 <aaron.gee@ryerson.ca>
2022-05-10 01:06:25 +0000

    Basic prompt format

--- src/Prompt.js
+++ src/Prompt.js
@@ -1,23 +1,31 @@
-import { Form, FormGroup, Label, Input, Button, Row } from "reactstrap";
+import {
+  Form,
+  FormGroup,
+  Label,
+  Input,
+  Button,
+  Row,
+  Container,
+  Col,
+} from "reactstrap";
 
 function Prompt() {
   return (
-    <div>
-      <Form>
-        <FormGroup>
-          <Row>
-            {" "}
-            <Label for="prompt">Enter prompt</Label>
-          </Row>
-          <Row>
-            <Input id="prompt" name="text" type="textarea" />
-          </Row>
-          <Row>
-            <Button>Submit</Button>
-          </Row>
-        </FormGroup>
-      </Form>
-    </div>
+    <Form>
+      <FormGroup>
+        <Row>
+          <Label className="text-left" for="prompt">
+            Enter prompt
+          </Label>
+        </Row>
+        <Row>
+          <Input id="prompt" name="text" type="textarea" />
+        </Row>
+        <Row>
+          <Button>Submit</Button>
+        </Row>
+      </FormGroup>
+    </Form>
   );
 }
 
